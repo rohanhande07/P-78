e5fec6db02f9794c2136f5d4f19860f05307b0e5
rohanhande07 <64739098+rohanhande07@users.noreply.github.com>
2022-02-19 10:32:10 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text,View } from 'react-native';
+import SignupLogin from './screens/SignupLogin.js';
+
+export default function App(){
+  return(
+    <View>
+      <SignupLogin/>
+    </View>
+  )
+}
